3fd1ba2a2c8a0aff84d08c3ebc229f235e17519c
Léo Feroge <leoferoge@gmail.com>
2025-09-25 11:43:44 +0200

    Envoie de mail ajouté

--- Projet_Progra/MainWindow.xaml.cs
+++ Projet_Progra/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +22,34 @@ namespace Projet_Progra
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string from = tbFrom.Text;
+                string password = pbPassword.Password;
+                string to = tbTo.Text;
+                string subject = tbSubject.Text;
+                string body = tbBody.Text;
+
+                MailMessage mail = new MailMessage(from, to, subject, body);
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.Credentials = new NetworkCredential(from, password);
+                client.EnableSsl = true;
+
+                client.Send(mail);
+
+                tbStatus.Text = "Mail envoyé avec succès";
+                tbStatus.Foreground = System.Windows.Media.Brushes.Green;
+            }
+            catch (Exception ex)
+            {
+                tbStatus.Text = "Erreur lors de l'envoi du mail: " + ex.Message;
+                tbStatus.Foreground = System.Windows.Media.Brushes.Red;
+            }
         }
     }
 }
